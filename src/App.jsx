3149ca66f33f70ba59d956c124b606650c465494
Ikhwan kurniawan <128669107+rhenaald@users.noreply.github.com>
2025-04-23 22:08:12 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,42 @@
-function App() {
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
+import Navbar from "./components/navbar";
+import Footer from "./components/footer";
+
+import HomePage from "./pages/HomePage";
+import About from "./pages/AboutPage";
+import Registration from "./pages/Authentication/Registration";
+import Login from "./pages/Authentication/Login";
+
+function LayoutWrapper() {
+  const location = useLocation();
+  const hideNavbarRoutes = ["/register", "/login"];
+  const hideFooterRoutes = ["/register", "/login"];
 
   return (
-    <div className="App">
-      <h1>Welcome</h1>
-    </div>
-  )
+    <>
+      {!hideNavbarRoutes.includes(location.pathname) && <Navbar />}
+
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/register" element={<Registration />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+
+      {!hideFooterRoutes.includes(location.pathname) && <Footer />}
+    </>
+    
+    
+  );
 }
 
-export default App
+function App() {
+  return (
+    <Router>
+      <LayoutWrapper />
+    </Router>
+  );
+}
+
+export default App;
