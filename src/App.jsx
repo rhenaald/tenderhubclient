495ea20dd0f3be08af10f2f9adb041a92cb1382d
Ikhwan kurniawan <128669107+rhenaald@users.noreply.github.com>
2025-04-30 18:02:43 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
-import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+} from "react-router-dom";
 import Navbar from "./components/navbar";
 import Footer from "./components/footer";
 
 import HomePage from "./pages/HomePage";
-import About from "./pages/AboutPage";
+import Projeck from "./pages/Projek";
 import Registration from "./pages/Authentication/Registration";
 import Login from "./pages/Authentication/Login";
+import DetailProjek from "./pages/DetailProjek";
+import ProfilePage from "./pages/Profile";
+import ActivityPage from "./pages/Activity";
 
 function LayoutWrapper() {
   const location = useLocation();
@@ -16,18 +24,18 @@ function LayoutWrapper() {
     <>
       {!hideNavbarRoutes.includes(location.pathname) && <Navbar />}
 
-
       <Routes>
+        <Route path="/detailprojek" element={<DetailProjek />} />
         <Route path="/" element={<HomePage />} />
-        <Route path="/about" element={<About />} />
+        <Route path="/projeck" element={<Projeck />} />
         <Route path="/register" element={<Registration />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/activity" element={<ActivityPage />} />
       </Routes>
 
       {!hideFooterRoutes.includes(location.pathname) && <Footer />}
     </>
-    
-    
   );
 }
 
