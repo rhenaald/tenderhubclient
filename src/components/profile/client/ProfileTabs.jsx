4a01d67c0726b9c646407dfff24e890bb205c374
Ikhwan kurniawan <128669107+rhenaald@users.noreply.github.com>
2025-05-11 22:17:39 +0700

    Update ProfileTabs.jsx

--- src/components/profile/client/ProfileTabs.jsx
+++ src/components/profile/client/ProfileTabs.jsx
@@ -1,13 +1,20 @@
 import React, { useState } from "react";
 import ProjectList from "./ProjectList";
 import HistoryList from "./HistoryList";
+import ActiveProjectList from "./ActiveProjectList";
 
 const ProfileTabs = ({ profileData, clientData }) => {
-    const [activeTab, setActiveTab] = useState("projects");
+    const [activeTab, setActiveTab] = useState("active");
 
     return (
         <div className="bg-white rounded-xl shadow-lg overflow-hidden">
             <div className="flex text-lg font-semibold text-gray-700 select-none border-b">
+                <button
+                    onClick={() => setActiveTab("active")}
+                    className={`py-4 px-6 ${activeTab === "active" ? "text-blue-500 font-bold border-b-2 border-blue-500" : "hover:bg-blue-50 transition duration-200"}`}
+                >
+                    Proyek Aktif
+                </button>
                 <button
                     onClick={() => setActiveTab("projects")}
                     className={`py-4 px-6 ${activeTab === "projects" ? "text-blue-500 font-bold border-b-2 border-blue-500" : "hover:bg-blue-50 transition duration-200"}`}
@@ -22,6 +29,7 @@ const ProfileTabs = ({ profileData, clientData }) => {
                 </button>
             </div>
 
+            {activeTab === "active" && <ActiveProjectList />}
             {activeTab === "projects" && <ProjectList />}
             {activeTab === "history" && <HistoryList />}
         </div>
