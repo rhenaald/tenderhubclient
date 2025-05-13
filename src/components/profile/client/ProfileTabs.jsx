48f8fef5416db3bf39b6acc3f336955ae6d5c1b7
Ikhwan kurniawan <128669107+rhenaald@users.noreply.github.com>
2025-05-13 11:23:56 +0700

    Update ProfileTabs.jsx

--- src/components/profile/client/ProfileTabs.jsx
+++ src/components/profile/client/ProfileTabs.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import ProjectList from "./ProjectList";
-import HistoryList from "./HistoryList";
 import ActiveProjectList from "./ActiveProjectList";
+import ReviewList from "./ReviewList";
 
 const ProfileTabs = ({ profileData, clientData }) => {
     const [activeTab, setActiveTab] = useState("active");
@@ -22,16 +22,16 @@ const ProfileTabs = ({ profileData, clientData }) => {
                     Proyek Saya
                 </button>
                 <button
-                    onClick={() => setActiveTab("history")}
-                    className={`py-4 px-6 ${activeTab === "history" ? "text-blue-500 font-bold border-b-2 border-blue-500" : "hover:bg-blue-50 transition duration-200"}`}
+                    onClick={() => setActiveTab("reviews")}
+                    className={`py-4 px-6 ${activeTab === "reviews" ? "text-blue-500 font-bold border-b-2 border-blue-500" : "hover:bg-blue-50 transition duration-200"}`}
                 >
-                    Riwayat
+                    Review
                 </button>
             </div>
 
             {activeTab === "active" && <ActiveProjectList />}
             {activeTab === "projects" && <ProjectList />}
-            {activeTab === "history" && <HistoryList />}
+            {activeTab === "reviews" && <ReviewList />}
         </div>
     );
 };
