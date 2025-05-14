a61da178d8d3c410ca3761b0f08c5b9ef23c3c36
Ikhwan kurniawan <128669107+rhenaald@users.noreply.github.com>
2025-05-14 17:13:24 +0700

    Update Projek.jsx

--- src/pages/Projek.jsx
+++ src/pages/Projek.jsx
@@ -120,7 +120,7 @@ export default function App() {
               {error}
             </div>
           ) : currentProjects.length === 0 ? (
-            <div className="bg-yellow-50 border border-yellow-200 text-yellow-700 px-4 py-8 rounded-lg text-center">
+            <div className="bg-yellow-50 border mt-30 border-yellow-200 text-yellow-700 px-4 py-8 rounded-lg text-center">
               <svg xmlns="http://www.w3.org/2000/svg" className="h-12 w-12 mx-auto mb-4 text-yellow-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M20 13V6a2 2 0 00-2-2H6a2 2 0 00-2 2v7m16 0v5a2 2 0 01-2 2H6a2 2 0 01-2-2v-5m16 0h-2.586a1 1 0 00-.707.293l-2.414 2.414a1 1 0 01-.707.293h-3.172a1 1 0 01-.707-.293l-2.414-2.414A1 1 0 006.586 13H4" />
               </svg>
